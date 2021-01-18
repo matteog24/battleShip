787f3106065e03d5498536331d3e4a0ff48ff74e
Matt <matteo.grz2006@gmail.com>
2021-01-18 08:32:54 +0000

    Updates

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Developed in JavaScript using NPM module: you'll need to use the command __npm init__ and after __npm install https://github.com/andreabeggiato-his/nodejs-input.git__, to allow the input from the keyboard.
 
-**Hope you will enjoy it!**
+*Hope you will enjoy it!*
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const SHIP_LENGTHS = [
 ];
 
 async function selectDifficulty() {
-  
+
   // return the difficulty of the game. You can choose to have only one difficulty
   // This function is async because we need keyboard input
 
@@ -17,40 +17,85 @@ async function selectDifficulty() {
 
 function fillComputerBoard(board) {
   // fill the computer board.
+
 }
 
-async function fillPlayerBoard(board, playerBoard) {
+async function fillPlayerBoard(board) {
+
+  let letterToNumber = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   // fill the computer board.
   // This function is async because we need keyboard input. You can also choose to
-  // create the playerBoard randomly.
-  let randomYesOrNo = await input.readInteger('Press 1 for a randomly generated board or 2 for choosing the specific coordinates.');
-  if (randomYesOrNo == '1') {
-    playerBoard[8][1] = 1;
-      utils.printBoard(playerBoard, false); 
-    fillPlayerBoardRandomly(fillPlayerBoard, SHIP_LENGTHS, playerBoard, board)
-  }
-  else if (randomYesOrNo == '2') {
-    // player selects the coordinates
-  }
-}
 
-function fillPlayerBoardRandomly(fillPlayerBoard, SHIP_LENGTHS, playerBoard, board) {
-  for (let a = 0; a < 10; a = a + 1) {
-  let a = Math.floor(Math.random() * SHIP_LENGTHS.length);
-  SHIP_LENGTHS.pop(SHIP_LENGTHS[a]);
-  let shipChoice = SHIP_LENGTHS[a];
+  for (let a = 0; a < SHIP_LENGTHS.length; a = a + 1) {
 
-  // select position of number
-  let randomPositionNumber = utils.getRandomNumber(1, 10);
-
-  // select position of the letters
-  let randomPositionLetter = utils.getRandomNumber(1, 10);
-  playerBoard[randomPositionNumber][randomPositionLetter] = 1;
-
-  }
   console.log('Here is your board');
-  utils.printBoard(playerBoard, false); 
+  utils.printBoard(board, false);
+  console.log();
+  console.log('Here are the ships avalible. The number represents the length.');
+
+  console.log()
+  console.log(...SHIP_LENGTHS.join(', '), '.')
+  console.log()
+
+  let shipChoice = await input.readInteger('Choose the ship');
+  SHIP_LENGTHS.splice(SHIP_LENGTHS.indexOf(shipChoice));
+  let coordinateNumber = await input.readInteger('Type the number coordinate');
+  let coordinateLetter = await input.readString('Type the letter coordinate');
+  board[coordinateNumber - 1][coordinateLetter - 1] = 1;
+
+  let secondCoordinateNumber;
+  let thirdCoordinateNumber;
+  let fourthCoordinateNumber;
+  let secondCoordinateLetter;
+  let thirdCoordinateLetter;
+  let fourthCoordinateLetter;
+
+  if (shipChoice === 2) {
+    secondCoordinateNumber = await input.readInteger('Type the second number coordinate');
+    secondCoordinateLetter = await input.readString('Type the second letter coordinate');
+    board[secondCoordinateNumber - 1][secondCoordinateLetter - 1] = 1;
+  }
+
+  else if (shipChoice === 3) {
+    thirdCoordinateNumber = await input.readInteger('Type the third number coordinate');
+    thirdCoordinateLetter = await input.readString('Type the third letter coordinate');
+
+    if (thirdCoordinateLetter === coordinateLetter) {
+      board[thirdCoordinateNumber - 2][thirdCoordinateLetter - 1] = 1;
+    }
+    else if (thirdCoordinateNumber === coordinateNumber) {
+      board[thirdCoordinateNumber - 1][thirdCoordinateLetter - 2] = 1;
+    }
+    else {
+      console.log('Something went wrong!');
+      return;
+    }
+    board[thirdCoordinateNumber - 1][thirdCoordinateLetter - 1] = 1;
+  }
+
+  else if (shipChoice === 4) {
+    fourthCoordinateNumber = await input.readInteger('Type the fourth number coordinate');
+    fourthCoordinateLetter = await input.readString('Type the fourth letter coordinate');
+    if (fourthCoordinateLetter === coordinateLetter) {
+      board[fourthCoordinateNumber - 3][fourthCoordinateLetter - 1] = 1;
+      board[fourthCoordinateNumber - 2][fourthCoordinateLetter - 1] = 1;
+    }
+    else if (fourthCoordinateNumber === coordinateNumber) {
+      board[fourthCoordinateNumber - 1][fourthCoordinateLetter - 3] = 1;
+      board[fourthCoordinateNumber - 1][fourthCoordinateLetter - 2] = 1;
+    }
+    else {
+      console.log('Something went wrong!');
+      return;
+    }
+    board[fourthCoordinateNumber - 1][fourthCoordinateLetter - 1] = 1;
+  }
+
+  utils.printBoard(board, false);
+  }
+
 }
+
 
 async function playerMove(computerBoard) {
   // update the playerBoard with the computer's move
@@ -74,15 +119,14 @@ async function main() {
 
   const difficulty = await selectDifficulty();
 
-  await fillPlayerBoard(playerBoard);
+  fillPlayerBoard(playerBoard);
   fillComputerBoard(computerBoard);
+  await playerMove(computerBoard)
+  computerMove(playerBoard);
 
-  playerMove(computerBoard)
-  computerMove(playerBoard)
-  
 
   // with this utils function you can print a board
-  utils.printBoard(playerBoard, false); 
+
   // while (!areAllShipsDestroyed(computerBoard) || !areAllShipsDestroyed(playerBoard)) {
   //   await playerMove(computerBoard);
   //   computerMove(playerBoard, difficulty);
